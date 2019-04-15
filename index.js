77d339a314778636428327e9aac9a5f9295cabcc
Jahaziel <jahaziel.vaz@gmail.com>
2019-04-15 09:59:53 -0600

    Now working on events

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-let nameInput = document.getElementById('name');
-let output = document.getElementById('output');
+// let nameInput = document.getElementById('name');
+// let output = document.getElementById('output');
+let body = document.getElementById('body');
+//
+// let mixer = {
+//   name: "Vincent"
+// }
+//
+// function finalRes(obj){
+//   obj.result = function(){
+//     return `Welcome ${this.name}`
+//   }
+// }
+//
+// finalRes(mixer);
+// output.innerHTML = mixer.result()
+window.addEventListener('compositionstart', () => {
+  return body.style = "background: blue";
+})
 
-let mixer = {
-  name: "Vincent"
-}
-
-function finalRes(obj){
-  obj.result = function(){
-    return `Welcome ${this.name}`
-  }
-}
-
-finalRes(mixer);
-output.innerHTML = mixer.result()
 
 
 
