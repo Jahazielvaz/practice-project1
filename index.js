82f963835d081d58a68f562600288338d2585a6d
Jahaziel <jahaziel.vaz@gmail.com>
2019-04-15 23:24:59 -0600

    I MADE A CAROUSEL ALL BY MYSELF THROUGH SHEER LOGIC

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let body = document.getElementById('body');
 let nameInput = document.getElementById('name');
 let buttonOne = document.getElementById('button1');
-
+let imageElement = document.createElement('img');
 
 // EVENT REMOVER SECTION
 let myEvent = () => {
@@ -20,21 +20,29 @@ nameInput.addEventListener('mouseleave', myEventRemover, false);
 
 // IMAGE SWITCHER CLICK AND DBLCLICK EVENT
 let imageContainer = document.getElementById("image-container");
-let imageElement = document.createElement('img');
+
 let images = ['./media-content/55-cancri-e.jpg', './media-content/blackhole.jpg', './media-content/magnetar.jpg'];
 let switcher = document.getElementById('switcher');
 
 function switcherFunc(){
   var i = 0;
-  i++
+  let shifter = images.shift();
+  images.push(shifter);
   imageElement.setAttribute('src', images[i]);
   imageContainer.appendChild(imageElement);
   // console.log(imageElement);
   console.log(images[i]);
 }
 
+function killSwitch(){
+  switcher.removeEventListener('click', switcherFunc)
+  imageContainer.removeChild(imageElement)
+}
 
 
+
+
+switcher.addEventListener('dblclick', killSwitch, false);
 switcher.addEventListener('click', switcherFunc, false);
 
 
